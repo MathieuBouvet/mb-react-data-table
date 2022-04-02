24a07bcaf860b8c863b9b91c85b4eec3eea92536
Mathieu Bouvet <mathieubouvet42@gmail.com>
2022-04-02 12:54:03 +0200

    Prepare project for publishing

---
+++ dist/ReactDataTable.js
@@ -0,0 +1,6 @@
+const ReactDataTable = () => {
+  return /*#__PURE__*/React.createElement("div", null, "Hello data table world ;)");
+};
+
+ReactDataTable.propTypes = {};
+export default ReactDataTable;
---
+++ src/lib/ReactDataTable.jsx
@@ -0,0 +1,7 @@
+const ReactDataTable = () => {
+  return <div>Hello data table world ;)</div>;
+};
+
+ReactDataTable.propTypes = {};
+
+export default ReactDataTable;
