8ea512f63f2e3d3838bad944fd0f8278f0d081f8
Mathieu Bouvet <mathieubouvet42@gmail.com>
2022-04-02 14:34:37 +0200

    Ignore dist folder

--- dist/ReactDataTable.js
+++
@@ -1,6 +0,0 @@
-const ReactDataTable = () => {
-  return /*#__PURE__*/React.createElement("div", null, "Hello data table world ;)");
-};
-
-ReactDataTable.propTypes = {};
-export default ReactDataTable;
